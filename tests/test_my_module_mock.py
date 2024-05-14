c7a509921e00ab6f138cc5942bac46c3d009d39d
Elie LECAS <elielecas00@gmail.com>
2024-05-14 10:52:52 +0200

    push main to dev

--- tests/test_my_module_mock.py
+++ tests/test_my_module_mock.py
@@ -4,7 +4,7 @@ import requests
 import pytest
 
 def test_f2_with_fixed_f1():
-    with patch('source.my_module_mock.f1', return_value=5) as mock_f1 :
+    with patch('source.my_module_mock.f1', return_value=50) as mock_f1 :
         assert f2() == 50
         mock_f1.assert_called_once()
 
